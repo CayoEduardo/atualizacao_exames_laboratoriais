db40fdce42a0f61439a10f555696e870bdbd96cc
Cayo <cayo.eduardo.silveira@gmail.com>
2022-04-15 03:45:59 -0300

    list_examples

--- src/server/client/lista_exames.php
+++ src/server/client/lista_exames.php
@@ -13,6 +13,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="lista_exames.css" />
     <title>Lista de amostras</title>
+    <script>
+      const goToCadastroJustificativa = () => {
+        window.location.href="http://localhost/url/src/server/client/cadastro_justificativa.php"
+      }
+    </script>
   </head>
   <body>
     <div class="wrapper">
@@ -31,6 +36,7 @@
               <th>Id Amostra</th>
               <th>Analista Clínico</th>
               <th>Supervisor</th>
+              <th></th>
             </tr>
             <?php foreach($exames as $index=>$exame): ?>
               <tr>
@@ -41,6 +47,12 @@
                 <td><?php echo $exame->getAmostra() !== NULL ? $exame->getAmostra()->getIdAmostra() : ''; ?></td>
                 <td><?php echo $exame->getAnalista() !== NULL ? $exame->getAnalista()->getNome() : ''; ?></td>
                 <td><?php echo $exame->getSupervisor() !== NULL ? $exame->getSupervisor()->getNome() : ''; ?></td>
+                <td>
+                <img src="check.png" width="14" height="14" />
+              </td>
+              <td onClick="goToCadastroJustificativa()">
+                <img src="refresh.png" width="14" height="14" />
+              </td>
             </tr>
             <?php endforeach; ?>
           </table>
