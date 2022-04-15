7a51e55124a20e7869aa445efb40d193beb49a66
pedroseabra <pedroseabra@estudante.ufscar.br>
2022-04-14 21:09:05 -0300

    fix format cadastrarAmostra

--- src/server/AddAmostra.php
+++ src/server/AddAmostra.php
@@ -2,7 +2,6 @@
     include_once(__DIR__.'/controllers/AmostraController.class.php');
 
     $controller = new AmostraController();
-    $dataColeta = new DateTime();
-    $result = $controller->cadastraAmostra(1, 'SANGUE', $dataColeta->format('YY-MM-DD h:I:S'), '');
-    var_dump($result);
+    $dataColeta = new DateTime('now');
+    $controller->cadastraAmostra(1, 'SANGUE', $dataColeta->format('Y-m-d h:i:s'), '46879049800');
 ?>
